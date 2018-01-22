1371dcbbc374da5ea2be392e4e834e2c1b5ede97
Roman Mezentsev <roman.mezentsev@gmail.com>
2018-01-22 23:58:16 +0700

    Fix imports in RxTask class.

--- src/main/kotlin/com/atiqus/rxtasks/RxTask.kt
+++ src/main/kotlin/com/atiqus/rxtasks/RxTask.kt
@@ -2,6 +2,7 @@ package com.atiqus.rxtasks
 
 import io.reactivex.Observable
 import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
